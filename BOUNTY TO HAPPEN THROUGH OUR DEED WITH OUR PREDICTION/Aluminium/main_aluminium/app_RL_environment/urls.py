65d8cddf17f22be9bc2f883fdaead514980bf3f0
AJAY M <132185651+Ajay18M@users.noreply.github.com>
2025-05-15 22:43:14 +0530

    Delete BOUNTY TO HAPPEN THROUGH OUR DEED WITH OUR PREDICTION/Aluminium/main_aluminium directory

--- BOUNTY TO HAPPEN THROUGH OUR DEED WITH OUR PREDICTION/Aluminium/main_aluminium/app_RL_environment/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('environment_home/', views.environment_home),
-    path('environment_register/', views.environment_register),
-    path('environment_login/', views.environment_login),
-    path('environment_logout/', views.environment_logout),
-]
